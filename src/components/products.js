ef570eb03f4bd58792d0a5564e344019931471b0
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-21 20:55:38 +0930

    products component created.

---
+++ src/components/products.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+    state = {  
+        products: []
+    }
+    render() { 
+        return ( 
+            
+         );
+    }
+}
+ 
+export default Products;
