7370e7952366fb28b009ad619bc881919c7c7a17
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-22 20:17:10 +0930

    account state and handle change function added.

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,44 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LoginForm extends Component {
-    state =  {
-        account: { username: '', password: '' }
-    };
+  state = {
+    account: { username: "", password: "" },
+  };
 
- 
-    handleSubmit = e => {
-        e.preventDefault();
+  handleSubmit = (e) => {
+      e.preventDefault();
+  };
 
-    }
-
-    handleChange = (e) => {
-        const account = { ...this.state.account };
-        account.username = e.currentTarget.value;
-        this.setState({ account });
-    };  
-    render() { 
-        return ( 
-            <div className="container">
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div className='form-group'>
-                        <label htmlFor='username'>Username</label>
-                        <input
-                            value={this.state.account.username}
-                            onChange={this.handleChange}
-                            id="username"
-                            type="text"
-                            className='form-control' />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='password'>Password</label>
-                        <input id='password' type='text' className='form-control' />
-                    </div>
-                    <button className='btn btn-primary'>Login</button>
-                </form>
-            </div>
-         );
-    }
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+  render() {
+    const { account } = this.state;
+    return (
+      <div className="container">
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginForm;
