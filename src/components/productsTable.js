e842984b6820be4a40903537ac6c8f4ce11d570a
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-22 13:25:04 +0930

    table body component created.

--- src/components/productsTable.js
+++ src/components/productsTable.js
@@ -1,57 +1,45 @@
 import React, { Component } from "react";
-import TableHeader from './common/tableHeader';
+import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
 import Like from "./common/like";
 
 class ProductsTable extends Component {
-    columns = [
-        { path: 'image', label: 'Image' },
-        { path: 'product', label: 'Product' },
-        { path: 'price', label: 'Price' },
-        { path: 'numberInStocks', label: 'Stocks' },
-        { path: 'dailyRentalRate', label: 'Rate' },
-        { key: 'like', label: 'Add to Wishlist' },
-        {},
-        { key: 'delete'}
-    ];
+  columns = [
+    { path: "image", label: "Image" },
+    { path: "product", label: "Product" },
+    { path: "price", label: "Price" },
+    { path: "numberInStock", label: "Stocks" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
+      content: product => <Like liked={product.liked} onClick={() => this.props.onLike(product)} />,
+      label: "Add to Wishlist",
+    },
+    // {
+    //   content: <button className="btn btn-success btn-sm">Add to Cart</button>,
+    // },
+    {
+      key: "delete",
+      content: product => (
+        <button
+          onClick={() => this.props.onDelete(product)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
   render() {
     const { products, onDelete, onLike, onSort, sortColumn } = this.props;
     return (
       <table className="table table-striped">
-            <TableHeader
-                columns={this.columns}
-                sortColumn={sortColumn}
-                onSort={onSort}
-            />
-        <tbody>
-          {products.map((product) => (
-            <tr key={product._id}>
-              <td>
-                <img src={product.picture} alt="product photo" />
-              </td>
-              <td>{product.product}</td>
-              <td>{product.price}</td>
-              <td>{product.numberInStock}</td>
-              <td>{product.dailyRentalRate}</td>
-              <td>
-                <Like
-                  liked={product.liked}
-                  onLikeToggle={() => onLike(product)}
-                />
-              </td>
-              <td>
-                <button className="btn btn-success btn-sm">Add to Cart</button>
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(product)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
+        <TableBody columns={this.columns} data={products} />
       </table>
     );
   }
