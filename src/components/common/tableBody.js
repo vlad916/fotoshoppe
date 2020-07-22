9d65e626e522e6d921626e3afd371bf6c0e84d19
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-22 13:28:18 +0930

    refractored prducts table component.

--- src/components/common/tableBody.js
+++ src/components/common/tableBody.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import _ from "lodash";
 
 class TableBody extends Component {
-    render() { 
-        const { data, columns } = this.props;
-        return ( 
-            <tbody>
-                {data.map(item => <tr>
-                    {columns.map(column => <td>{_.get(item, column.path)}</td>)} 
-                </tr>)}
-              
-            </tbody>
-         );
-    }
+  renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+
+    return _.get(item, column.path);
+  };
+
+  render() {
+    const { data, columns } = this.props;
+    return (
+      <tbody>
+        {data.map((item) => (
+          <tr>
+            {columns.map((column) => (
+              <td>{this.renderCell(item, column)}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
 }
- 
+
 export default TableBody;
