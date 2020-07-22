e394502e87234d67a79d5b8505dc7969e980e15b
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-22 20:25:26 +0930

    input component created.

---
+++ src/components/common/input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Input = (props) => {
+    const { name, label, value, onChange } = props;
+    return (
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <input
+                value={value}
+                onChange={onChange}
+                id="username"
+                name="username"
+                type="text"
+                className="form-control"
+            />
+        </div>
+     );
+}
+ 
+export default Input;
