12daa5ca4655409007193334ea3e2fe6493593d9
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-22 11:51:48 +0930

    genre select function created.

---
+++ src/components/common/listGroup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ListGroup extends Component {
+    render() { 
+        return ( null );
+    }
+}
+ 
+export default ListGroup;
--- src/components/products.js
+++ src/components/products.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import { getProducts } from "../services/productsService";
+import { getGenres } from "../services/categoryService";
+import { paginate } from "../utils/paginate";
 import Like from "./common/like";
+import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
-import { paginate } from '../utils/paginate';
+
 import "./products.css";
 
 class Products extends Component {
   state = {
-    products: getProducts(),
+    products: [],
+    genres: [],
     currentPage: 1,
     pageSize: 4,
   };
+
+  componentDidMount() {
+    this.setState({ products: getProducts(), genres: getGenres() });
+  }
 
   handleDelete = (product) => {
     const products = this.state.products.filter((p) => p._id !== product._id);
@@ -28,68 +36,79 @@ class Products extends Component {
   handlePageChange = (page) => {
     this.setState({ currentPage: page });
   };
+    
+    handleGenreSelect = (genre) => {
+
+    };
 
   render() {
     const { length: count } = this.state.products;
-    const { pageSize, currentPage, products: allProducts } = this.state;
+    const { pageSize, currentPage, products: allProducts, genres } = this.state;
 
-      if (count === 0) return <p>There are no products in the database.</p>;
-      
-      const products = paginate(allProducts, currentPage, pageSize)
+    if (count === 0) return <p>There are no products in the database.</p>;
+
+    const products = paginate(allProducts, currentPage, pageSize);
     return (
-      <React.Fragment>
-        <p>Showing {count} products...</p>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Stocks</th>
-              <th>Rate</th>
-              <th>Add to Wishlist</th>
-                        <th></th>
-                        <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product._id}>
-                <td>
-                  <img src={product.picture} alt='product photo' />
-                </td>
-                <td>{product.product}</td>
-                <td>{product.price}</td>
-                <td>{product.numberInStock}</td>
-                <td>{product.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={product.liked}
-                    onLikeToggle={() => this.handleLike(product)}
-                  />
-                    </td>
-                    <td>
-                        <button className='btn btn-success btn-sm'>Add to Cart</button>
-                    </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(product)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
+      <div className="row">
+        <div className="col-2">
+                <ListGroup items={genres} onItemSelect={this.handleGenreSelect}/>
+        </div>
+        <div className="col">
+          <p>Showing {count} products...</p>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Stocks</th>
+                <th>Rate</th>
+                <th>Add to Wishlist</th>
+                <th></th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          itemsCount={count}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        />
-      </React.Fragment>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>
+                    <img src={product.picture} alt="product photo" />
+                  </td>
+                  <td>{product.product}</td>
+                  <td>{product.price}</td>
+                  <td>{product.numberInStock}</td>
+                  <td>{product.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={product.liked}
+                      onLikeToggle={() => this.handleLike(product)}
+                    />
+                  </td>
+                  <td>
+                    <button className="btn btn-success btn-sm">
+                      Add to Cart
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(product)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
+        </div>
+      </div>
     );
   }
 }
