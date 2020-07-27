cd13dab24d65a77e183784b98faa37e32c5db918
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-27 11:12:24 +0930

    created seed.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send("We are online");
-});
+// app.get('/', (req, res) => {
+//     res.send("We are online");
+// });
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
