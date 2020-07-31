d53eb728a50f6f5d8f0dc9bc327357f3bb002f52
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-31 14:02:52 +0930

    Updated navbar component.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,14 +2,18 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import "./css/navbar.css";
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
   return (
     <div id="header">
       <nav className="navbar navbar-dark navbar-expand-sm">
         <div className="container-fluid">
           <span className="navbar-text">
             <Link className="nav-link" to="/">
-              <img className="img-fluid logo" src="assets/logo.png" alt="Responsive image"/>
+              <img
+                className="img-fluid logo"
+                src="assets/logo.png"
+                alt="Responsive image"
+              />
               <span className="text">Fotoshoppe</span>
             </Link>
           </span>
@@ -24,22 +28,40 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbar">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <NavLink className="nav-link" to="/products">
+                <NavLink className="nav-item nav-link" to="/products">
                   Products
                 </NavLink>
               </li>
+              {!user && (
+                <React.Fragment>
+                  <li>
+                    <NavLink className="nav-item nav-link" to="/login">
+                      Sign In
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="nav-item nav-link" to="/register">
+                      Sign Up
+                    </NavLink>
+                  </li>
+                </React.Fragment>
+              )}
+              {user && (
+                <React.Fragment>
+                  <li>
+                    <NavLink className="nav-item nav-link" to="/profile">
+                      {user.name}
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="nav-item nav-link" to="/logout">
+                      Sign Out 
+                    </NavLink>
+                  </li>
+                </React.Fragment>
+              )}
               <li>
-                <NavLink className="nav-link" to="/login">
-                  Sign In
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to="/register">
-                  Sign Up
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" to="/cart">
+                <NavLink className="nav-item nav-link" to="/cart">
                   <i className="fas fa-shopping-cart"></i>
                 </NavLink>
               </li>
