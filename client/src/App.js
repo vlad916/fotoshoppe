da4a98d66ebae51745eaef3852be915876e5150d
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-07-31 13:57:50 +0930

    Function to get current user created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Products from './components/products';
-// import ProductsForm from './components/productsForm';
 import Navbar from './components/navbar';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
 import NotFound from './components/notFound';
+import Logout from "./components/logout";
+import auth from "./services/authService";
 import ShoppingCart from './components/shoppingCart';
 
 import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-    <Navbar />
-      <main className="container-fluid mt-5">
-        <Switch>
-          <Route path='/cart' component={ShoppingCart}></Route>
-          <Route path='/register' component={RegisterForm}></Route>
-          <Route path='/login' component={LoginForm}></Route>
-          {/* <Route path='/products:id' component={ProductsForm}></Route> */}
-          <Route path="/products" component={Products}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from='/' exact to='/products' />
-          <Redirect to='/not-found' />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
-}
+class App extends Component {
+  state = {};
 
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <React.Fragment>
+        <Navbar user={user} />
+        <main className="container-fluid mt-5">
+          <Switch>
+            <Route path="/cart" component={ShoppingCart}></Route>
+            <Route path="/register" component={RegisterForm}></Route>
+            <Route path="/login" component={LoginForm}></Route>
+            <Route path="/logout" component={Logout} />
+            <Route path="/products" component={Products}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/products" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 export default App;
