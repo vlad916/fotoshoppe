2995efece139b6557b6dc7a738b92a7fc8208982
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-08-04 13:07:03 +0930

    updated pagination component

--- client/src/components/common/pagination.js
+++ client/src/components/common/pagination.js
@@ -9,7 +9,7 @@ const Pagination = (props) => {
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
   return (
-    <nav style={{ marginLeft: "880px" }}>
+    <nav>
       <ul className="pagination">
         {pages.map((page) => (
           <li
